71cd3104c181036940d94f5eb49c500a1858db72
kadriyeaksakal <kadriyeaksakal.ka@gmail.com>
2022-05-17 01:50:46 +0300

    [ENHANCE] writings page update with api

--- src/pages/Writings.vue
+++ src/pages/Writings.vue
@@ -10,15 +10,15 @@
     <section class="contact-content detail">
       <div class="container">
         <div class="row q-col-gutter-lg justify-center" >
-          <div class="col-sm-3" v-for="content in writing.attributes.writingContent" :key="content.id">
+          <div class="col-sm-3" v-for="content in writingList" :key="content.id">
             <div class="contact-content  detail-card">
-              <img :src="`http://localhost:1337${content.media}`" :alt="content.mediaAltText"/>
+              <img :src="`http://localhost:1337${content.attributes.media.data.attributes.url}`" :alt="content.attributes.mediaAltText"/>
               <div class="info">
-                <label>{{content.title}}</label>
-                <span>{{content.letter.slice(0,200)}}</span><br/>
-                <a href="/mobileitm-scm/">
+                <label>{{content.attributes.title}}</label>
+                <span>{{content.attributes.letter.slice(0,150)}}</span><br/>
+                <router-link :to="$route.path + '/' + content.id">
                   Daha Fazlası
-                </a>
+                </router-link>
               </div>
             </div>
           </div>
@@ -50,17 +50,29 @@ export default {
   data() {
     return {
       writingInfo: [],
+      writingList: [],
       error: null
     }
   },
   mounted() {
     this.getWritingInfo()
+    this.getWritingList()
   },
   methods: {
+    async getWritingList() {
+      this.writingList = []
+      try {
+        const response = await this.$axios.get('http://localhost:1337/api/writing-contents?populate=*')
+        this.writingList = response.data.data
+        //console.log('sss', this.writingInfo)
+      } catch (error) {
+        this.error = error
+      }
+    },
     async getWritingInfo() {
       this.writingInfo = []
       try {
-        const response = await this.$axios.get('http://localhost:1337/api/writings?populate=*')
+        const response = await this.$axios.get('http://localhost:1337/api/writings?populate[writingContent][populate]=*')
         this.writingInfo = response.data.data
         //console.log('sss', this.writingInfo)
       } catch (error) {
