7bf52ca732807beddc65e34e4738947c8a79d464
kadriyeaksakal <kadriyeaksakal.ka@gmail.com>
2022-05-17 01:57:20 +0300

    [ENHANCE] add show image control for banner

--- src/components/Banner/Banner.vue
+++ src/components/Banner/Banner.vue
@@ -2,8 +2,8 @@
   <section class="banner">
     <div class="container">
       <div class="row full-width">
-        <div class="col-sm-6 col-xs-12">
-          <img  :class="imgClass" :src="require(`../../assets/avatar.jpeg`)"/>
+        <div class="col-sm-6 col-xs-12" >
+          <img v-if="imgClass" :class="imgClass" :src="require(`../../assets/avatar.jpeg`)"/>
         </div>
         <div class="col-sm-6 col-xs-12">
           <div class="text" :class="textClass">
